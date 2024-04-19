c4d69eaaba6e5414071fc1089220ffe475f5915d
unknown <mahsuma.rezai8@gmail.com>
2024-04-19 10:38:48 -0700

    add list of html

---
+++ component/Html/Listes.js
@@ -0,0 +1,14 @@
+
+const List = (prosp) => {
+    return (
+        <div>
+            <button>HTML Introduction</button>
+            <button>HTML Elements</button>
+
+
+
+        </div>
+    )
+}
+
+export default List
--- component/Html/htmlinto.js
+++ component/Html/htmlinto.js
@@ -1,12 +1,15 @@
 import { Fragment } from 'react';
 import classes from './Html.module.css';
 import Head from '../Head/Head';
+import List from './Listes';
 const Htmlinto = (props) => {
    
     return (
         <Fragment>
             <Head/>
             <div className={classes.warrp}>
+            <h1> WHAT IS HTML ?</h1>
+             <List/>
             </div>
 
 
