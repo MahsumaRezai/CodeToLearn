9261242cf184dcfd2c50b6cc3ad59e6adce9478b
unknown <mahsuma.rezai8@gmail.com>
2024-04-22 00:09:03 -0700

    add last for 22

--- component/Html/HtmlLesson1.js
+++ component/Html/HtmlLesson1.js
@@ -5,7 +5,7 @@ import Example from './ex';
 const HtmlLesson1 = (props) => {
     return (
         <Fragment>
-            <Htmlhead/>
+            <Htmlhead />
             <div className={classes.into}>
                 <div className={classes.list}>
                     <div className={classes.title}>
@@ -29,7 +29,29 @@ const HtmlLesson1 = (props) => {
                 </div>
 
                 <div>
-                    <Example/>
+                    <Example />
+                </div>
+
+                <div className={classes.list}>
+                    <div> <p className={classes.text}>Example Explained
+
+                    </p></div>
+
+                    <ul className={classes.ullist}>
+                        <li> The &lt;!DOCTYPE html &gt; declaration defines this document to be HTML5 </li>
+                        <li> The  &lt;html&gt; element is the root element of an HTML page</li>
+                        <li>The &lt;head&gt; element contains meta information about the document</li>
+                        <li>
+                            The &lt;title&gt; element specifies a title for the document
+
+                            </li>
+                        <li>The &lt;body&gt; element contains the visible page content</li>
+                        <li>The &lt;h1&gt; element defines a large heading
+                                          </li>
+                        <li>The &lt;p&gt; element defines a paragraph
+                        </li>
+                    </ul>
+
                 </div>
 
 
