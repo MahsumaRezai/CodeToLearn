bca8fa2aa99b0654193e6ff68bc569b8ca3abafb
unknown <mahsuma.rezai8@gmail.com>
2024-04-21 23:04:00 -0700

    add head for html for back

---
+++ component/Head/Htmlhead.js
@@ -0,0 +1,13 @@
+import {Fragment} from 'react';
+import classes from './Htmlhead.module.css'
+const Htmlhead=(props)=>{
+    return (
+        <Fragment>
+            <div className={classes.head}>
+
+            </div>
+
+        </Fragment>
+    )
+}
+export default Htmlhead
