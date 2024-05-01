f136055cca2390b2a6c89f88e7cb596144cbcd2c
unknown <mahsuma.rezai8@gmail.com>
2024-05-01 10:53:02 -0700

    add last 22

--- component/Html/Card.js
+++ component/Html/Card.js
@@ -3,7 +3,7 @@ import classes from './HtmlLesson1.module.css';
 import ParaText from './paratext';
 import Example from './ex'
 import Tip from './Tip';
-import HtmlList from './HtmlList';
+import ListPara from './ListPara';
 
 
 const Card = (props) => {
@@ -14,14 +14,32 @@ const Card = (props) => {
 
 
 "   /> 
- <Example html="&lt; !DOCTYPE html&gt;" head=" &lt;head&gt;" title="&lt;title&gt;
-                     Page Title" titleend="  &lt;/title&gt;" headend="  &lt;/head&gt;" body="&lt;body&gt;"  h1="   &lt;h1&gt;
-                     This is the html page" h1end=" &lt;/h1&gt;" para="  &lt;p&gt;
-                     This is a  Html Paragraphs" prarend="  &lt;/p&gt;" bodyend="  &lt;/body&gt;" htmlend="    &lt;/html&gt;" 
-                     headtitle="A Simple HTML Paragraphs"/>
+    <Example html="&lt; !DOCTYPE html&gt;" head=" &lt;head&gt;" title="&lt;title&gt;
+                        Page Title" titleend="  &lt;/title&gt;" headend="  &lt;/head&gt;" body="&lt;body&gt;"  h1="   &lt;h1&gt;
+                        This is the html page" h1end=" &lt;/h1&gt;" para="  &lt;p&gt;
+                        This is a  Html Paragraphs" prarend="  &lt;/p&gt;" bodyend="  &lt;/body&gt;" htmlend="    &lt;/html&gt;" 
+                        headtitle="A Simple HTML Paragraphs"/>
 
-                     <Tip title=" Browsers automatically add some white space (a margin) before and after a paragraph."/>
-                     <HtmlList/>
+                        <Tip title=" Browsers automatically add some white space (a margin) before and after a paragraph."/>
+                        <ListPara title="HTML Display" list1="You cannot be sure how HTML will be displayed." list2="Large or small screens,
+                         and resized windows will create different results." list3="With HTML, you cannot change the output by adding extra spaces or
+                          extra lines in your HTML code." list4="The browser will remove any extra spaces and extra lines when the page is displayed:" />
+                        <Example html="&lt; !DOCTYPE html&gt;" head=" &lt;head&gt;" title="&lt;title&gt;
+                        Page Title" titleend="  &lt;/title&gt;" headend="  &lt;/head&gt;" body="&lt;body&gt;"  h1="   &lt;h1&gt;
+                        This is the html page" h1end=" &lt;/h1&gt;" para="  &lt;p&gt;
+                        This paragraph
+    contains a lot of lines
+    in the source code,
+    but the browser
+    ignores it." prarend="  &lt;/p&gt;" bodyend="  &lt;/body&gt;" htmlend="    &lt;/html&gt;" 
+                        headtitle="A Example"/>
+            <ListPara title="HTML Line Breaks" list1="The HTML <br> element defines a line break."
+             list2="Use <br> if you want a line break (a new line) without starting a new paragraph:"
+              list3="The <br> tag is an empty tag, which means that it has no end tag." list4="The HTML <pre> element defines preformatted text."
+ 
+              />
+
+
 
             </div>
         </Fragment >
---
+++ component/Html/ListPara.js
@@ -0,0 +1,23 @@
+import { Fragment } from 'react';
+import classes from './HtmlLesson1.module.css';
+
+const ListPara = (props) => {
+    return (
+        <Fragment>
+            <div className={classes.list}>
+                <div> <h1 className={classes.texthead}>{props.title}
+                </h1></div>
+                <ul className={classes.ullist}>
+                    <li>{props.list1}</li>
+                    <li>{props.list2}</li>
+                    <li>{props.list3}</li>
+                    <li>{props.list4}</li>
+                </ul>
+               
+
+            </div>
+
+        </Fragment>
+    )
+}
+export default ListPara;
