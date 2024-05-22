b263dc7c00f14a71501903ef98585aed4aecbdbb
unknown <mahsuma.rezai8@gmail.com>
2024-05-21 23:00:50 -0700

    work on table html section

--- component/Html/Table.js
+++ component/Html/Table.js
@@ -1,48 +1,124 @@
 import { Fragment } from 'react';
-import classes from './table.module.css'
+import classes from './table.module.css';
+import { useRouter } from 'next/router';
+import Button from './Button';
+import Buttontow from './Buttontow'
+import Tip from './Tip';
 const Table = (props) => {
-    return (
-        <Fragment>
-            <h2>HTML Table</h2>
+    const router = useRouter('');
 
-            <table className={classes.table}>
-                <tr>
-                    <th>Company</th>
-                    <th>Contact</th>
-                    <th>Country</th>
-                </tr>
-                <tr>
-                    <td>Alfreds Futterkiste</td>
-                    <td>Maria Anders</td>
-                    <td>Germany</td>
-                </tr>
-                <tr>
-                    <td>Centro comercial Moctezuma</td>
-                    <td>Francisco Chang</td>
-                    <td>Mexico</td>
-                </tr>
-                <tr>
-                    <td>Ernst Handel</td>
-                    <td>Roland Mendel</td>
-                    <td>Austria</td>
-                </tr>
-                <tr>
-                    <td>Island Trading</td>
-                    <td>Helen Bennett</td>
-                    <td>UK</td>
-                </tr>
-                <tr>
-                    <td>Laughing Bacchus Winecellars</td>
-                    <td>Yoshi Tannamuri</td>
-                    <td>Canada</td>
-                </tr>
-                <tr>
-                    <td>Magazzini Alimentari Riuniti</td>
-                    <td>Giovanni Rovelli</td>
-                    <td>Italy</td>
-                </tr>
-            </table>
-        </Fragment>
+    const nextpage = () => {
+        router.push('/HtmlLesson4')
+    }
+
+    const HtmlLesson4 = () => {
+        router.push('/HtmlLink')
+    }
+    return (
+
+        <Fragment>
+            <div className={classes.into}>
+                <div className={classes.list}>
+                    <div className={classes.btn}>
+                        <Button title="❮ Previous" onClick={nextpage} ></Button>
+                        <Buttontow title="Next ❯" onClick={HtmlLesson4}></Buttontow>
+                    </div>
+                    <div className={classes.title}>
+                        <h1>Defining an HTML Table</h1>
+                    </div>
+
+                    <div className={classes.instedlist}>
+                        <div className={classes.tabletex}> <p className={classes.text}>
+                            An HTML table is defined with the &lt;table&gt; tag.
+
+                 </p>
+                            <p>
+                                Each table row is defined with the &lt;tr&gt; tag. A table header is defined with the &lt;th&gt;
+                                 tag. By default, table headings are bold and centered. A table data/cell is defined with the &lt;td&gt; tag.
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+                <div className={classes.warrpe}>
+                    <div className={classes.insed}>
+
+                        &lt;table&gt;
+                        <br></br>
+                        <br></br>
+
+                        &lt;tr&gt;
+                        <br></br>
+                        <br></br>
+
+                        &lt;th&gt;Firstname&lt;/th
+                        <br></br>
+                        <br></br>
+
+                        &lt;th&gt;Lastname&lt;/th&gt;
+                        <br></br>
+                        <br></br>
+
+                        &lt;th&gt;Age&lt;/th&gt;
+                        <br></br>
+                        <br></br>
+
+                        &lt;/tr&gt;
+                        <br></br>
+                        <br></br>
+
+                        &lt;tr&gt;
+                        <br></br>
+                        <br></br>
+
+                        &lt;td&gt;Jill&lt;/td&gt;
+                        <br></br>
+                        <br></br>
+
+                        &lt;td&gt;Smith&lt;/td&gt;
+                        <br></br>
+                        <br></br>
+
+                        &lt;td&gt;50&lt;/td&gt;
+                        <br></br>
+                        <br></br>
+
+                        &lt;/tr&gt;
+                        <br></br>
+                        <br></br>
+
+                        &lt;tr&gt;
+                        <br></br>
+                        <br></br>
+
+                        &lt;td&gt;Eve&lt;/td&gt;
+                        <br></br>
+                        <br></br>
+
+                        &lt;td&gt;Jackson&lt;/td&gt;
+                        <br></br>
+                        <br></br>
+
+                        &lt;td&gt;94&lt;/td&gt;
+                        <br></br>
+                        <br></br>
+
+                        &lt;/tr&gt;
+                        <br></br>
+                        <br></br>
+
+
+                        &lt;/table&gt;
+                </div>
+
+                </div>
+                <Tip title=" Note : The <td> elements are the data containers of the table.
+                 They can contain all sorts of HTML elements; text, images, lists, other tables, etc."/>
+                 
+
+            </div >
+
+        </Fragment >
     )
 }
 export default Table;
